b37fa332241a051598015cd65c11f4d66c10a74b
sadewa <andi.prastyawan@privyid.tech>
2025-02-03 22:46:12 +0700

    create pokemon_class.py and add requirements.txt

---
+++ pokemon_class.py
@@ -0,0 +1,18 @@
+from pydantic import BaseModel
+from typing import List
+
+class PokemonAttributes(BaseModel):
+    name: str
+    height: float
+    weight: float
+    types: List[str]
+    base_experience: int
+    sprite_url: str
+
+class PokemonData(BaseModel):
+    type: str = "pokemon"
+    id: str
+    attributes: PokemonAttributes
+
+class PokemonResponse(BaseModel):
+    data: List[PokemonData]
